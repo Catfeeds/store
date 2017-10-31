6acbd33a1fbf054818ef3bf0fc593566bf1e25ce
DevZeng <devzeng@sennki.com>
2017-10-31 09:50:53 +0800

    fix

--- app/Console/Commands/fixAddress.php
+++ app/Console/Commands/fixAddress.php
@@ -48,36 +48,36 @@ class fixAddress extends Command
         $cities = $data['result'][1];
         $dist = $data['result'][2];
         for ($i=0;$i<count($provinces);$i++){
-            $province = new City();
-            $province ->id = $provinces[$i]['id'];
-            $province ->pid = 0;
-            $province ->name = $provinces[$i]['fullname'];
-            $province ->latitude = $provinces[$i]['location']['lat'];
-            $province ->longitude = $provinces[$i]['location']['lng'];
-            $province->save();
-            echo "FINISH".$province->name."\n";
+//            $province = new City();
+//            $province ->id = $provinces[$i]['id'];
+//            $province ->pid = 0;
+//            $province ->name = $provinces[$i]['fullname'];
+//            $province ->latitude = $provinces[$i]['location']['lat'];
+//            $province ->longitude = $provinces[$i]['location']['lng'];
+//            $province->save();
+            echo "FINISH I".$i."\n";
             if (isset($provinces[$i]['cidx'])){
                 $province_cities = array_slice($cities,$provinces[$i]['cidx'][0],$provinces[$i]['cidx'][1]);
                 for ($j = 0;$j<count($province_cities);$j++){
-                    $province_city = new City();
-                    $province_city -> id = $province_cities[$j]['id'];
-                    $province_city -> pid = $provinces[$i]['id'];
-                    $province_city -> name = $province_cities[$j]['fullname'];
-                    $province_city -> latitude = $province_cities[$j]['location']['lat'];
-                    $province_city -> longitude = $province_cities[$j]['location']['lng'];
-                    echo "FINISH".$province_city->name."\n";
-                    $province_city->save();
-                    if (isset($province_city[$j]['cidx'])){
-                        $city_dists = array_slice($dist,$province_city[$j]['cidx'][0],$province_city[$j]['cidx'][1]);
+//                    $province_city = new City();
+//                    $province_city -> id = $province_cities[$j]['id'];
+//                    $province_city -> pid = $provinces[$i]['id'];
+//                    $province_city -> name = $province_cities[$j]['fullname'];
+//                    $province_city -> latitude = $province_cities[$j]['location']['lat'];
+//                    $province_city -> longitude = $province_cities[$j]['location']['lng'];
+                    echo "FINISH J".$j."\n";
+//                    $province_city->save();
+                    if (isset($province_cities[$j]['cidx'])){
+                        $city_dists = array_slice($dist,$province_cities[$j]['cidx'][0],$province_cities[$j]['cidx'][1]);
                         for($k=0;$k<count($city_dists);$k++){
-                            $dist = new City();
-                            $dist->id = $city_dists[$k]['id'];
-                            $dist->pid = $province_city[$j]['id'];
-                            $dist->name = $city_dists[$k]['fullname'];
-                            $dist->latitude = $city_dists[$k]['location']['lat'];
-                            $dist->longitude = $city_dists[$k]['location']['lng'];
-                            $dist->save();
-                            echo "FINISH".$dist->name."\n";
+//                            $dist = new City();
+//                            $dist->id = $city_dists[$k]['id'];
+//                            $dist->pid = $province_city[$j]['id'];
+//                            $dist->name = $city_dists[$k]['fullname'];
+//                            $dist->latitude = $city_dists[$k]['location']['lat'];
+//                            $dist->longitude = $city_dists[$k]['location']['lng'];
+//                            $dist->save();
+                            echo "FINISH $k".$k."\n";
                         }
                     }
                 }
