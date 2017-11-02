aea3f6856fa20ebe034f1461bc5b6e1f76981df1
DevZeng <devzeng@sennki.com>
2017-11-02 15:37:56 +0800

    fix

--- app/Http/Controllers/API/V1/CommodityController.php
+++ app/Http/Controllers/API/V1/CommodityController.php
@@ -610,7 +610,7 @@ class CommodityController extends Controller
         for ($i=0;$i<count($data);$i++){
             $data[$i]->picture = $data[$i]->pictures()->pluck('thumb_url')->first();
             $dist = calculateDistance($data[$i]->latitude,$data[$i]->longitude,$lat,$lng);
-            $data[$i]->dist = round($dist,4)*1000;
+            $data[$i]->dist = round($dist,3)*1000;
         }
         return $data;
     }
