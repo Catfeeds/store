2396293b230d7440a65d9e2fa83767525c1103c9
DevZeng <devzeng@sennki.com>
2017-11-03 10:56:20 +0800

    add city count

--- app/Http/Controllers/API/V1/CommodityController.php
+++ app/Http/Controllers/API/V1/CommodityController.php
@@ -632,7 +632,7 @@ class CommodityController extends Controller
     {
         $cid = Input::get('city_id');
         $cid_group = City::where('pid','=',$cid)->pluck('id');
-        $city_group = Commodity::whereIn('city_id',$cid_group)->groupBy('city_id')->count();
+        $city_group = Commodity::whereIn('city_id',$cid_group)->selectRaw('count(*) as number')->groupBy('city_id');
         dd($city_group);
     }
 }
