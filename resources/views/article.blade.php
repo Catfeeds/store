8cfce9e37546d549f7ead0671b721950c225a4c6
ZENG <zeng@ZENGdeiMac.lan>
2017-11-10 22:31:17 +0800

    fix

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0,user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{url('css/css.quill.bubble.css')}}">
+    <link rel="stylesheet" href="{{url('css/quill.bubble.css')}}">
     <title>{{$article->title}}</title>
 </head>
 
