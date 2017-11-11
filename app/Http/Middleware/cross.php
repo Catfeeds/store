bb18bcba9e29abd07119d9f8c78536be6d5b0e5f
DevZeng <devzeng@sennki.com>
2017-11-11 11:56:51 +0800

    fix cross

--- app/Http/Middleware/cross.php
+++ app/Http/Middleware/cross.php
@@ -16,7 +16,8 @@ class cross
     public function handle($request, Closure $next)
     {
         $response = $next($request);
-        $response->header('Access-Control-Allow-Origin', 'http://192.168.3.44:8080');
+//        $response->header('Access-Control-Allow-Origin', 'http://192.168.3.44:8080');
+        $response->header('Access-Control-Allow-Origin', 'http://119.23.255.177:8092');
         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
         $response->header('Access-Control-Allow-Methods', 'GET,POST,PATCH,PUT,OPTIONS,DELETE');
         $response->header('Access-Control-Allow-Credentials', 'true');
