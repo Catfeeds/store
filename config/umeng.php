55e438d4032e1843d91dd3d840ae4d8653a4ed53
DevZeng <devzeng@sennki.com>
2017-11-13 11:42:27 +0800

    fix

--- config/umeng.php
+++ config/umeng.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'ios_app_key'                => '',
-    'ios_app_master_secret'     => '',
+    'ios_app_key'                => '59eb446af43e483fc700000c',
+    'ios_app_master_secret'     => 'hxqnjzgcfihcv2rmbuxfpbrutxqh9o6l',
     'android_app_key'            => '59e96c48310c933440000080',
     'android_app_master_secret' => 's2on8euoqfw09o4iruonqqztoghwgnjr',
     'production_mode'                =>  env('UMENG_MODE', true)
