baac2fbc495e6827af4b93135830daddbf86ad6a
DevZeng <devzeng@sennki.com>
2017-11-14 16:59:53 +0800

    fix

--- app/Http/Controllers/API/V1/CommodityController.php
+++ app/Http/Controllers/API/V1/CommodityController.php
@@ -861,7 +861,7 @@ class CommodityController extends Controller
             $commodity->where('created_at','>',$start)->where('created_at','<',$end);
             $count = $commodity->count();
         }
-        $data = $commodity->limit($limit)->offset(($page-1)*$limit)->get();
+        $data = $commodity->limit($limit)->offset(($page-1)*$limit)->orderBy('id','DESC')->get();
         if (!empty($data)){
             for ($i=0;$i<count($data);$i++){
                 $type = CommodityType::find($data[$i]->type);
@@ -910,7 +910,7 @@ class CommodityController extends Controller
             $commodity->where('created_at','>',$start)->where('created_at','<',$end);
             $count = $commodity->count();
         }
-        $data = $commodity->limit($limit)->offset(($page-1)*$limit)->get();
+        $data = $commodity->limit($limit)->offset(($page-1)*$limit)->orderBy('id','DESC')->get();
         if (!empty($data)){
             for ($i=0;$i<count($data);$i++){
                 $type = CommodityType::find($data[$i]->type);
