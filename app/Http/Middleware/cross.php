77af6f4381c31d53318c78293389ca20201a4100
DevZeng <devzeng@sennki.com>
2017-11-21 17:58:48 +0800

    fix

--- app/Http/Middleware/cross.php
+++ app/Http/Middleware/cross.php
@@ -18,7 +18,7 @@ class cross
         $response = $next($request);
 //        $response->header('Access-Control-Allow-Origin', 'http://192.168.3.44:8080');
 //        $response->header('Access-Control-Allow-Origin', 'http://119.23.255.177:8092');
-        $response->header('Access-Control-Allow-Origin', 'http://www.ankekan.com');
+        $response->header('Access-Control-Allow-Origin', 'http://admin.ankekan.com');
         $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Cookie, Accept');
         $response->header('Access-Control-Allow-Methods', 'GET,POST,PATCH,PUT,OPTIONS,DELETE');
         $response->header('Access-Control-Allow-Credentials', 'true');
