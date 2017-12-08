98f4111a1acf0662230d4417574d22fa7ce87f19
DevZeng <devzeng@sennki.com>
2017-12-08 17:38:27 +0800

    fix

--- config/alisms.php
+++ config/alisms.php
@@ -2,8 +2,11 @@
 return [
     'KEY' =>'24647928',
     'SECRETKEY'=>'818d589230a5e38b0b2e668c69ba6ec6',
-    'company'=>'飞鸟看看',
+    'company'=>'安珂看看',
     'VerificationCode'=>'SMS_109410181',
-    'Pass'=>'SMS_109515216',
-    'Fail'=>'SMS_109450243'
+//    'Pass'=>'SMS_109515216',
+    'Pass'=>'SMS_116515037',
+//    'Fail'=>'SMS_109450243',
+    'Fail'=>'SMS_116385024'
+
 ];
