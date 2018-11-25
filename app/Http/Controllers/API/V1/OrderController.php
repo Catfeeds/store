1be21f299f1753938361a3e3dc7014d497fcdb3b
DevZeng <zengshungeng@live.com>
2018-11-25 18:48:18 +0800

    gix

--- app/Http/Controllers/API/V1/OrderController.php
+++ app/Http/Controllers/API/V1/OrderController.php
@@ -634,8 +634,7 @@ class OrderController extends Controller
             'desc' => '帐户提现',                  //付款说明
         ];
         $config = config('wxxcx');
-        $pay = new Pay($config);
-        $result =  $pay->driver('wechat')->gateway('app')->transfer($order);
+        $result =  Pay::wechat($config)->transfer($order);
         return response()->json([
             'return_code'=>'SUCCESS',
             'data'=>$result
